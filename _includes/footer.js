99ec784b245bef9f6ba82a19e1d84681c07992c3
Tyler Wittig <44188028+twit96@users.noreply.github.com>
2026-05-25 21:54:24 -0500

    Remove TW logo from footer

--- _includes/footer.js
+++ _includes/footer.js
@@ -2,6 +2,7 @@ document.write(`
   <footer>
     <div class="wrapper">
       <b>Juan Gonzalez De Mendoza</b>
+      
       <nav>
         <a href="https://github.com/JuanGMendoza">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
@@ -20,21 +21,17 @@ document.write(`
         </a>
       </nav>
 
-      <!-- T.Wittig Site Design Attribution -->
-      <a href="https://twit96.github.io/" id="tw-attribution">
-        <img src="https://tylerwittig.com/img/logo.png" alt="TW Logo" />
-        Design by TW
-      </a>
-
     </div>
   </footer>
 `);
-/*        <a href="https://www.youtube.com/channel/UC560ZaRh8BrtR00J4dH7PgA">
+
+/*
+        <!-- move me between the a tags above for YouTube link in footer -->
+        <a href="https://www.youtube.com/channel/UC560ZaRh8BrtR00J4dH7PgA">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
             <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
             <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/>
           </svg>
           YouTube
         </a>
-
-        */
+*/
